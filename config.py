bf4fc93760ec73c4c81007e9a629afea26700384
mutyasowmya <sowmya.mutya@slu.edu>
2025-07-01 20:52:22 -0500

    testing workflows

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'mysql+pymysql://root:rootpass@localhost/permits')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'mysql+pymysql://root:Sowmya@123@localhost/permits')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret')
