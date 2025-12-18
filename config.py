e41209f49315ffc8e6614263524cd44126987c2f
MutyaSowmya123 <sowmya.mutya@slu.edu>
2025-12-18 10:44:27 -0600

    updated database connectivity and docker files

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'mysql+pymysql://root:Sowmya@123@localhost/permits')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'postgresql://username:password@lUHJRpermits')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret')
--- run.py
+++ run.py
@@ -1,12 +1,21 @@
+import time
+from sqlalchemy.exc import OperationalError
 from app import create_app, db
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 
 app = create_app()
 
 with app.app_context():
-    db.create_all()
+    for i in range(10):
+        try:
+            db.create_all()
+            print("Database ready")
+            break
+        except OperationalError:
+            print("Waiting for database...")
+            time.sleep(2)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
